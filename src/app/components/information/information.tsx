dae029a31775a7d7fe64db73f1aab3be2ee8d6e7
Lerry Augusto <lerry.pirani@gmail.com>
2025-02-28 17:38:29 -0300

    fix: deleting unused svg

--- src/app/components/information/information.tsx
+++ src/app/components/information/information.tsx
@@ -1,18 +1,28 @@
-import { SectionTitle } from "../section-title/section-title"
-import "./information.scss"
+import { BrazilIcon } from "../icons/bra-icon";
+import { UsaIcon } from "../icons/usa-icon";
+import { SectionTitle } from "../section-title/section-title";
+import "./information.scss";
 
-export function Info(){
-    return(
-        <div className="infos">
-        <SectionTitle text="Languages" />
-        <div className="languages-info">
-          <span>🇺🇸 EN - Intermediate</span>
-          <span>🇧🇷 PT-BR - Native Speaker</span>
-        </div>
-        <SectionTitle text="Education" />
-        <div className="educational-info"></div>
-        <span>🎓</span>
-        <span>Technologist in Analysis and development of systems - Faculdade de Tecnologia de Franca</span>
+export function Info() {
+  return (
+    <div className="infos">
+      <SectionTitle text="Languages" />
+      <div className="languages-info">
+        <span>
+          <UsaIcon /> EN - Intermediate
+        </span>
+        <span>
+          <BrazilIcon /> PT-BR - Native Speaker
+        </span>
       </div>
-    )
+      <SectionTitle text="Education" />
+      <div className="educational-info">
+        <span>🎓</span>
+        <span>
+          Technologist in Analysis and development of systems - Faculdade de
+          Tecnologia de Franca
+        </span>
+      </div>
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
 import { Header } from "./components/header/header";
-import "./../styles/home.scss"
+import "./components/styles/home.scss";
 import { Experience } from "./components/experience/experience";
 import { Info } from "./components/information/information";
+import { EmailIcon } from "./components/icons/email-icon";
+import { SocialBtns } from "./components/social-btns/social-btns";
 
 export default function Home() {
   return (
     <main className="container">
-      <Header/>
-      <Experience/>
-      <Info/>
+      <Header />
+      <Experience />
+      <Info />
       <div className="buttons">
         <div className="social"></div>
-        <button>contac me!</button>
+        <SocialBtns />
+        <button className="btn-primary">
+          contact me!
+          <EmailIcon />
+        </button>
       </div>
-
     </main>
   );
 }

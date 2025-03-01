733a73b7c32ce8539c2dd8e85bcab53e81748678
Lerry Augusto <lerry.pirani@gmail.com>
2025-03-01 19:10:09 -0300

    feat: finish experience and layout page

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,17 +1,92 @@
-import { SectionTitle } from "../section-title/section-title"
-import "./experience.scss"
+import { SectionTitle } from "../section-title/section-title";
+import "./experience.scss";
+import Image from "next/image";
 
-export function Experience(){
-    return(
-        <div className="experience">
-        <SectionTitle text="Experience"/>
-        <p>
-          I´m a software engineer with 3 years of experience, working with
-          frontend and backend technologies.
-        </p>
-        <div className="experience-time">
-
+export function Experience() {
+  return (
+    <div className="experience">
+      <SectionTitle text="Experience" />
+      <p>
+        I´m a software engineer with 3 years of experience, working with
+        frontend and backend technologies.
+      </p>
+      <div className="experience-time">
+        <div className="experience-language">
+          <Image
+            aria-hidden
+            src="/dart.png"
+            alt="Dart Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-3">
+              <span>3 years</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            aria-hidden
+            src="/flutter.png"
+            alt="Flutter Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-3">
+            <span>3 years</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            aria-hidden
+            src="/java.png"
+            alt="Java Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">
+            <span>3 years</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            aria-hidden
+            src="/react.png"
+            alt="React Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">
+            <span>3 years</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            aria-hidden
+            src="/ts.png"
+            alt="Typescript Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">
+            <span>3 years</span>
+            </div>
+          </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import {Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.scss";
 
 const poppins = Poppins({
   subsets: ['latin'], 
-  weight: ['300','400','500', '600']
-})
-
+  weight: ['300', '400', '500', '600']
+});
 
 export const metadata: Metadata = {
   title: "Lerry Augusto",
-  description: "Lerry Augusto is a software engineer and this is my portifolio .",
+  description: "Lerry Augusto is a software engineer and this is my portfolio.",
 };
 
 export default function RootLayout({
@@ -20,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins}`}>
+      <body className={poppins.className}>
         {children}
       </body>
     </html>
